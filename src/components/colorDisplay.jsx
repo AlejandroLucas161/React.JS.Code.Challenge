16a2a8fe658c1b3d4cf97916ab130316b21bf8de
AlejandroLucas161 <alejandrolucas161@gmail.com>
2022-01-09 16:56:36 -0300

    DisplayColor InputColor

---
+++ src/components/colorDisplay.jsx
@@ -0,0 +1,20 @@
+const ColorDisplay = ({ colorValue, hexValue, isDarkText }) => {
+  return (
+    <section
+      className="colorDisplay"
+      style={{
+        backgroundColor: colorValue,
+        color: isDarkText ? '#000' : '#FFF',
+      }}
+    >
+      <p>{colorValue ? colorValue : 'Empty Value'}</p>
+      <p>{hexValue ? hexValue : null}</p>
+    </section>
+  );
+};
+
+ColorDisplay.defaultProps = {
+  colorValue: 'Empty Color Value',
+};
+
+export default ColorDisplay;
